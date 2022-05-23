f63bc69db58af8fd0374020501cc2424c8f218bf
fherrerao <fherrera0206@gmail.com>
2022-05-23 15:00:00 -0500

    Add routes for users with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :groups
+  resources :home_page, only: [:index]  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "groups#index"
+  root "home_page#index"
 end
