494ee04acac751a7ac8bc0a714ae600fe73daae9
fherrerao <fherrera0206@gmail.com>
2022-05-23 15:45:16 -0500

    Add seedf with a user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+first_user = User.new(name: 'Kasuki', email: 'first@email.com', :password => '123456', :password_confirmation => '123456')
+first_user.skip_confirmation!
+first_user.save!
