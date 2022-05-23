6c7a602736478e8a86b904e02e3d68b5c68e02ba
fherrerao <fherrera0206@gmail.com>
2022-05-23 15:44:55 -0500

    Add name field to sign_up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  validates :name, uniqueness: { case_sensitive: false },
+         presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }  
   
 end
