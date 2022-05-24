a9e2e307f2fb04c18f7e5d0c2b59687c87f434d8
fherrerao <fherrera0206@gmail.com>
2022-05-23 22:59:52 -0500

    Add method admin to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
 
-  validates :name, uniqueness: { case_sensitive: false },
-         presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }  
-  
+  validates :name, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }  
+  has_many :groups, dependent: :destroy
+  has_many :entities, dependent: :destroy
+
+  def admin?
+    role == 'admin'
+  end  
 end
