c32304484694e6f71d02f7e2fda522215bbb1661
fherrerao <fherrera0206@gmail.com>
2022-05-25 18:30:23 -0500

    Add class_name and foreign keys to has_many relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
 
   validates :name, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }  
-  has_many :groups, dependent: :destroy
-  has_many :entities, dependent: :destroy
+  
+  has_many :groups,  class_name: "Group", foreign_key: "author_id", dependent: :delete_all
+  has_many :entities,  class_name: "Entity", foreign_key: "author_id", dependent: :delete_all
 
   def admin?
     role == 'admin'
