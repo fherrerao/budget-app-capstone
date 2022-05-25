1192bc9f33925c3d23f97f82839aac1790617830
fherrerao <fherrera0206@gmail.com>
2022-05-24 20:09:10 -0500

    Add routes for transactions and views for groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :groups
   resources :home_page, only: [:index]  
+  resources :groups do 
+    resources :entities
+  end  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
