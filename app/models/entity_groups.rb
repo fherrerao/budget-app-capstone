af905ea4663c8ca376cd68dbb1118c0aeb96f9ba
fherrerao <fherrera0206@gmail.com>
2022-05-24 20:07:13 -0500

    Add associations to entity and group

---
+++ app/models/entity_groups.rb
@@ -0,0 +1,4 @@
+class EntityGroups < ApplicationRecord  
+  belongs_to :entity
+  belongs_to :group
+end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
   has_one_attached :icon
-  belongs_to :user
-  has_many :entity_groups
+  belongs_to :user  
+  has_many :entity_groups,  class_name: "EntityGroups", foreign_key: "group_id", dependent: :delete_all
 end
