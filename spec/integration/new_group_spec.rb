940de81705e47d5906af7ee908beabeb9b43da50
fherrerao <fherrera0206@gmail.com>
2022-05-26 14:45:27 -0500

    Add integration test to add a new group

---
+++ spec/integration/new_group_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'New Group', type: :system do
+  before(:all) do
+    User.destroy_all
+    Group.destroy_all
+
+    @user = User.new(name: 'Kasuki', email: 'first@email.com', password: '123456', password_confirmation: '123456')
+    @user.skip_confirmation!
+    @user.save!    
+  end
+
+  describe 'create new category' do
+    before:each do
+      visit new_user_session_path
+      fill_in 'Email', with: 'first@email.com'
+      fill_in 'Password', with: '123456'
+      click_button 'LOG IN'
+      click_link 'NEW CATEGORY'
+    end
+
+    it 'should display the new category page' do
+      expect(page).to have_current_path(new_group_path)
+    end
+
+    it 'should have a form to create a new category' do
+      expect(page).to have_selector('form')
+    end
+
+    it 'should have a name field' do
+      expect(page).to have_field('Name')
+    end    
+
+    it 'should have a button to create a new category' do
+      expect(page).to have_button('NEW CATEGORY')
+    end
+
+    it 'is not possible to create a new category without a name' do
+      fill_in 'Name', with: ''
+      click_button 'NEW CATEGORY'
+      expect(page).to have_content('Name can\'t be blank')
+    end
+
+  end
+end
