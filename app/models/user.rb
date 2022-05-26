97438de8ed30eaa8646277d708aa37db0581cbca
fherrerao <fherrera0206@gmail.com>
2022-05-26 18:54:58 -0500

    Fix confimable to Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   validates :name, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false,
                    format: { with: /\A[a-zA-Z0-9]+\z/ }
