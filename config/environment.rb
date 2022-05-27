0e9f40ed187222b6b32ec01aef14a36c06a5ab1b
fherrerao <fherrera0206@gmail.com>
2022-05-27 00:04:30 -0500

    Add default url to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,5 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailer.default_url_options = { host: 'https://fierce-brook-65336.herokuapp.com/' }
