c9a152db6a0c1a02054301e58565b75bbbde7e9f
fherrerao <fherrera0206@gmail.com>
2022-05-27 10:56:27 -0500

    Fix bug of sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def after_sign_in_path_for(resource)
-  #   stored_location_for(resource) || groups_path
-  # end
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || groups_path
+  end
 
   protected
 
