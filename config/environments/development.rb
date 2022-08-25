9b2dcbb1efa60b0d1dc8a6aae83e46d8b170b556
fherrerao <fherrera0206@gmail.com>
2022-08-25 17:55:45 -0500

    Setting up the mail in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,9 +39,21 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: Rails.application.credentials.dig(:gmail, :email),
+    password: Rails.application.credentials.dig(:gmail, :password),
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -68,6 +80,5 @@ Rails.application.configure do
   # config.action_view.annotate_rendered_view_with_filenames = true
 
   # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_cable.disable_request_forgery_protection = true  
 end
