36b93298c006cce366ec0b452e714d107786e548
fherrerao <fherrera0206@gmail.com>
2022-08-26 09:28:30 -0500

    Setting confirmation by email in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,24 +64,24 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "budget_app_capstone_production"
 
-  # config.action_mailer.perform_caching = false
-  # config.action_mailer.raise_delivery_errors = true  
-  # config.action_mailer.default_url_options = { host: 'https://budget-app-cap.herokuapp.com/'}
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.default :charset => 'utf-8'
-  # config.action_mailer.default_options = { from: Rails.application.credentials.dig(:gmail, :email) }
+  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true  
+  config.action_mailer.default_url_options = { host: 'https://budget-app-cap.herokuapp.com/'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => 'utf-8'
+  config.action_mailer.default_options = { from: Rails.application.credentials.dig(:gmail, :email) }
   
-  # config.action_mailer.smtp_settings = {
-  #   address: 'smtp.gmail.com',
-  #   port: 587,
-  #   domain: 'gmail.com',
-  #   user_name: Rails.application.credentials.dig(:gmail, :email),
-  #   password: Rails.application.credentials.dig(:gmail, :password),
-  #   authentication: 'plain',
-  #   enable_starttls_auto: true,
-  #   openssl_verify_mode: 'none'
-  # }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: Rails.application.credentials.dig(:gmail, :email),
+    password: Rails.application.credentials.dig(:gmail, :password),
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    openssl_verify_mode: 'none'
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
