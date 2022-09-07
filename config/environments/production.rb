a2c5cc0809096d58287e39e98f5f82a92b5c6c27
fherrerao <fherrera0206@gmail.com>
2022-09-07 14:58:31 -0500

    Change the production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true  
-  config.action_mailer.default_url_options = { host: 'https://budget-app-cap.herokuapp.com/'}
+  config.action_mailer.default_url_options = { host: 'https://budget-app.fly.dev/'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => 'utf-8'
